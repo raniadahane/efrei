8383727550cd4f2833da6f5a9c8eba060ac1d31a
rdahane <60604855+raniadahane@users.noreply.github.com>
2023-11-22 11:26:11 +0100

    Initial commit

---
+++ src/main/java/efrei/example/efrei/EfreiApplication.java
@@ -0,0 +1,13 @@
+package efrei.example.efrei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfreiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfreiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/efrei/example/efrei/EfreiApplicationTests.java
@@ -0,0 +1,13 @@
+package efrei.example.efrei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfreiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
