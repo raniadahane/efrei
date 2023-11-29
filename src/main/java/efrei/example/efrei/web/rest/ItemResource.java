dc8ed22e51bcfb84c2095796b074b3356dcd28f4
rdahane <60604855+raniadahane@users.noreply.github.com>
2023-11-29 12:06:05 +0100

    Entity created

--- src/main/java/efrei/example/efrei/web/rest/ItemResource.java
+++ src/main/java/efrei/example/efrei/web/rest/ItemResource.java
@@ -1,14 +1,28 @@
 package efrei.example.efrei.web.rest;
 
+
+import efrei.example.efrei.domain.Item;
+import efrei.example.efrei.service.ItemService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ItemResource {
+
+    public final ItemService itemService;
+
+    public ItemResource(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
     @GetMapping("/items")
-    public String test(){
-        return "Hello World !";
+    public List<Item> test() {
+        return itemService.findAll();
     }
 }
